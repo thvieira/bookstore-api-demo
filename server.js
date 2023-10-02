f434bbc120fcaa3b3adeed63a15227fa497c58a9
Thomas Vieira <thvieira.rs@gmail.com>
2023-10-02 02:36:49 -0300

    Primeira versão: CRUD de livros.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// import http from "http";
+import 'dotenv/config';
 import app from "./src/app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log("servidor escutando!");
+    console.log(`Server running on ${PORT}...`);
 });
